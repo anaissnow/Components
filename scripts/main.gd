b5701fb347e1ae1bf00fd4b500d81db93e470733
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2025-12-27 20:43:10 +0800

    parallax bg town

--- scripts/main.gd
+++ scripts/main.gd
@@ -112,3 +112,7 @@ func _on_restart_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_pause_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/option menu/option_menu.tscn")
