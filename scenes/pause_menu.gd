3a105b371c680ba793e196a2f4809258f0c5ee61
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2025-12-28 01:03:53 +0800

    restart button bug fixed

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -15,8 +15,8 @@ func _on_main_menu_button_pressed() -> void:
 
 
 func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
 	get_tree().call_deferred("reload_current_scene")
-
 
 
 func _on_resume_button_pressed() -> void:
