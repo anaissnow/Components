895d4d7c40d674e429e72a0e8217735264d5af38
Anais Snow <80048741+anaissnow@users.noreply.github.com>
2026-02-11 18:42:35 +0800

    created forest

--- Top-Down/Scripts/player_top_down.gd
+++ Top-Down/Scripts/player_top_down.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
-@export var speed : int = 400
+@export var speed : int = 150
+@onready var animated_sprite_2d : AnimatedSprite2D = $AnimatedSprite2D
+
 
 func get_input():
-	var input_direction = Input.get_vector("up", "down", "left", "right")
+	var input_direction := Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	if input_direction:
+		if velocity.x >= 1:
+			animated_sprite_2d.flip_h = true
+			animated_sprite_2d.play("walking")
+		if velocity.x <= 1:
+			animated_sprite_2d.flip_h = false
+			animated_sprite_2d.play("walking")
+	print(input_direction)
 
 func _physics_process(delta: float) -> void:
 	get_input()
